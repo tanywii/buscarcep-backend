bc85b1be6413d54afc9e233bcb9778fca2b49f10
Witany do Nascimento <152197555+tanywii@users.noreply.github.com>
2024-05-15 10:59:55 -0300

    Update ViaCepClientIntegration.java
    adicionados commits para facilitar a leitura do código

--- src/main/java/com/br/projetocep/integration/ViaCepClientIntegration.java
+++ src/main/java/com/br/projetocep/integration/ViaCepClientIntegration.java
@@ -5,13 +5,14 @@ import org.springframework.web.client.RestTemplate;
 
 import com.br.projetocep.domain.CepDomain;
 
+// É A CLASSE QUE SE COMUNICA COM A API DO VIACEP 
 public class ViaCepClientIntegration {
 
 	String url = "https://viacep.com.br/ws/parametro/json/";
 	
 	public CepDomain buscarEndereco(String cepInformado) {
-		RestTemplate restTemplate = new RestTemplate();		
-		ResponseEntity<CepDomain> cepDomain  = restTemplate.getForEntity(url.replace("parametro", cepInformado), CepDomain.class);
-		return cepDomain.getBody();
+		RestTemplate restTemplate = new RestTemplate();	 // CRIA UM OBJETO DO TIPO RestTemplate
+		ResponseEntity<CepDomain> cepDomain  = restTemplate.getForEntity(url.replace("parametro", cepInformado), CepDomain.class); // FAZ A CHAMADA PARA O VIACEP E CAPTURA O RETORNO
+		return cepDomain.getBody(); //RETORNA A CLASSE CepDomain COM OS DADOS DO VIACEP
 	}
 }
