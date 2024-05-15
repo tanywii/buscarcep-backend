a9a8fae4943b2195f17890422096da3da08c3340
Witany do Nascimento <152197555+tanywii@users.noreply.github.com>
2024-05-15 13:27:16 -0300

    Update ProjetocepApplication.java
    add comentarios

--- src/main/java/com/br/projetocep/ProjetocepApplication.java
+++ src/main/java/com/br/projetocep/ProjetocepApplication.java
@@ -4,6 +4,7 @@ package com.br.projetocep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// CLASSE MAIN DO JAVA, PADRAO DO SPRING
 @SpringBootApplication
 public class ProjetocepApplication {
 
