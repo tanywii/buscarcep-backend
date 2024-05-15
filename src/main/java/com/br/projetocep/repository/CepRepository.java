d2815f51e6e142da01513038cde6f7f292584363
Witany do Nascimento <152197555+tanywii@users.noreply.github.com>
2024-05-15 13:17:27 -0300

    Update CepRepository.java
    Add comentarios para facilitar leitura

--- src/main/java/com/br/projetocep/repository/CepRepository.java
+++ src/main/java/com/br/projetocep/repository/CepRepository.java
@@ -1,9 +1,11 @@
-package com.br.projetocep.repository;
+package com.br.projetocep.repository; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.br.projetocep.entity.EnderecoEntity;
 
+// ESSA CLASSE É A COMIICAÇÃO ENTRE A CLASSE EnderecoEntity E O Banco de dados 
+//APENAS EXTENDE A CLASSE DO JPA E NOS PERMITE USAR MẼTODOS DO JPA
 public interface CepRepository extends JpaRepository<EnderecoEntity, Long>  {
 
 }
